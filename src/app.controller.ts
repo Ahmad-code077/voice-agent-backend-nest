b8304513c11d917ff928a2f7d54f6aac4983d286
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2026-01-31 03:26:47 +0500

    fix: clean up formatting in AppController and AppModule

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,8 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger';
 @ApiTags('App')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
-
+  constructor(private readonly appService: AppService) {}
+  hel() {}
   @Get()
   @ApiOperation({ summary: 'Get hello message' })
   getHello(): string {
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CallsModule } from './calls/calls.module';
+import { IntegrationsModule } from './integrations/integrations.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
+    CallsModule,
+    IntegrationsModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
